3a7644075b109c1b869a2034543ca5465cc62cec
luiskrlosfr <lc.figueroar26@gmail.com>
2018-02-27 18:44:25 -0600

    Teams js added to folder

---
+++ app/assets/javascripts/teams/teams.js
@@ -0,0 +1,23 @@
+let arrColors = ["#bdbdbd", "#d12020", "#f59126", "#ff85b6", "#56cfcf", "#47962c", "#000000", "#e7e147"];
+let arrLetra = ["#ffffff", "#ffffff", "#ffffff", "#ffffff", "#ffffff", "#ffffff", "#ffffff", "#ffffff"];
+function hover(team) {
+  team.style.color = arrColors[team.id];
+  team.style.background = "white";
+  team.style.cursor = 'pointer';
+}
+
+function mouseOut(team) {
+  team.style.color = arrLetra[team.id];
+  team.style.background = arrColors[team.id];
+  team.style.cursor = 'pointer';
+}
+
+function colorCircles(circles) {
+  for (var i = 0; i <  circles; i++) {
+    let cir = document.getElementById(i);
+    cir.style.background = arrColors[i];
+    cir.style.color = arrLetra[i];
+    cir.setAttribute('onmouseover', "hover(this)");
+    cir.setAttribute('onmouseout', "mouseOut(this)");
+  }
+}
