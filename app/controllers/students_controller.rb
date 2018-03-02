93046e34015e0b368314d76d30294381ba7e556b
luiskrlosfr <lc.figueroar26@gmail.com>
2018-03-02 12:05:17 -0600

    Student index displays unasigned students

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,6 +1,7 @@
 class StudentsController < ApplicationController
   def index
-    @students = Student.all
+    @team = Team.find_by(teamName: "Sin Asignar")
+    @students = Student.where("team_id = ?", @team)
   end
 
 end
