3b5329c8cad972c987287bb08657e6abdd5a5691
luiskrlosfr <lc.figueroar26@gmail.com>
2018-03-02 11:13:04 -0600

    Team shows students with format

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -7,4 +7,13 @@ class TeamsController < ApplicationController
     @team = Team.find(params[:id])
     @members = @team.students
   end
+
+  private
+  def update_student
+    puts "HOLAAAAAAAAAAAAAAAAA"
+    @student = Student.find_by(params[:id])
+    @student.update(team: nil)
+    redirect_to team_path
+  end
+
 end
